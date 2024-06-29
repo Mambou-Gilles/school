c29d2bf0acef77ef91b9f20f0e7628cd3d9af68f
Mambou-Gilles <gillesravelmambou@gmail.com>
2024-06-30 01:53:44 +0200

    addig cors

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const dotenv = require("dotenv").config();
 const routes = require("./routes")
 const mongodb = require('./config/db.config');
+const cors = require("cors");
 
 const port = process.env.PORT;
 const host = process.env.HOST
 
-app.use(express.json());
-
-app.use('/', routes);
+app.use(express.json())
+    .use(cors())
+    .use('/', routes);
 
 app.listen(port, () => {
     console.log(`Server is running at ${host}:${port}`)
