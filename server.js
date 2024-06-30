4cc2c0beb238d340d099c5610e6331072f8f8fbd
Mambou-Gilles <gillesravelmambou@gmail.com>
2024-06-30 02:59:42 +0200

    well

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.json())
     .use(cors())
     .use('/', routes);
 
+process.on('uncaughtException', (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+
 app.listen(port, () => {
     console.log(`Server is running at ${host}:${port}`)
 })
